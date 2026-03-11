9987fa6ea32072f88f6ce058cc423834199c02cd
Orion Li <373595331@qq.com>
2026-03-11 16:03:11 +0800

    logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -130,7 +130,7 @@ const config: Config = {
       title: "Orion's Site",
       logo: {
         alt: "My Site Logo",
-        src: "img/logo.svg",
+        src: "img/logo.png",
       },
       items: [
         {
